d107b6fc076aa4f53b69203dabbeb5ab6fcfa7cc
Sukkoth <suukootj@gmail.com>
2023-08-08 16:17:05 +0300

    started single product page

---
+++ src/components/Product/ProductDetail.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './productDetail.css';
+
+const ProductDetail = () => {
+    return <div className='main'>ProductDetail</div>;
+};
+
+export default ProductDetail;
