dd4d27ac8693fbf99aac3d2ac0b8569413894feb
Sukkoth <suukootj@gmail.com>
2023-08-08 15:37:36 +0300

    products page listing

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,10 +1,17 @@
 import { FaCartPlus, FaSearch, FaHeart } from 'react-icons/fa';
 import phone1 from '../../assets/products/headphone-1.jpg';
-
-const Product = () => {
+const staticImageUrl = 'http://localhost:8000/images/products';
+const Product = ({ product }) => {
     return (
         <div className='product'>
-            <img src={phone1} alt='product image' className='img' />
+            <img
+                src={
+                    `${staticImageUrl}/${product?.variations[0]?.images[0]}` ||
+                    phone1
+                }
+                alt='product image'
+                className='img'
+            />
             <div className='product-menu'>
                 <FaSearch className='icons' />
                 <FaCartPlus className='icons' />
--- src/features/Products/productSlice.js
+++ src/features/Products/productSlice.js
@@ -4,7 +4,7 @@ import axios from '../../utils/axios';
 const initialState = {
     products: {
         isLoading: false,
-        error: {},
+        error: '',
         data: [],
         queryOptions: {
             page: 1,
@@ -13,23 +13,33 @@ const initialState = {
     },
     featuredProducts: {
         isLoading: false,
-        error: {},
+        error: '',
         data: [],
     },
 };
 
-export const fetchProducts = createAsyncThunk(
-    'blogs/fetchBlogs',
-    async (_, { getState }) => {
-        const response = await axios.get(`/products`);
-        return response.data;
-    }
-);
+export const fetchProducts = createAsyncThunk('blogs/fetchBlogs', async () => {
+    const response = await axios.get(`/products`);
+    return response.data;
+});
 
 const productsSlice = createSlice({
     name: 'products',
     initialState,
     extraReducers: (builder) => {
-        builder.addCase();
+        builder.addCase(fetchProducts.pending, (state) => {
+            state.products.isLoading = true;
+            state.products.error = '';
+        });
+        builder.addCase(fetchProducts.fulfilled, (state, action) => {
+            state.products.isLoading = false;
+            state.products.data = action.payload;
+        });
+        builder.addCase(fetchProducts.rejected, (state, action) => {
+            state.products.isLoading = false;
+            state.products.error = action.error;
+        });
     },
 });
+
+export default productsSlice;
