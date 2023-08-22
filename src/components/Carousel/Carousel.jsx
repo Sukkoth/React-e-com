afee7cd922eec3cf704f42a7b560f109cfb187a6
Sukkoth <suukootj@gmail.com>
2023-08-22 13:32:42 +0300

    fixed carousel not showing images

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,19 +1,25 @@
-import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import './swiper.css'; //custom
+import carouselPic1 from '../../assets/products/tv-1.jpg';
+import carouselPic2 from '../../assets/products/tv-2.jpg';
+import carouselPic3 from '../../assets/products/phone-1.jpg';
+import carouselPic4 from '../../assets/products/phone-2.jpg';
+import carouselPic5 from '../../assets/products/tablet-1.jpg';
+import carouselPic6 from '../../assets/products/tablet-2.jpg';
+
 const Carousel = () => {
-    const [images, setImages] = useState([
-        'tv-1.jpg',
-        'tv-2.jpg',
-        'phone-1.jpg',
-        'phone-2.jpg',
-        'tablet-1.jpg',
-        'tablet-2.jpg',
-    ]);
+    const images = [
+        carouselPic1,
+        carouselPic2,
+        carouselPic3,
+        carouselPic4,
+        carouselPic5,
+        carouselPic6,
+    ];
     return (
         <Swiper
             navigation={true}
@@ -26,10 +32,7 @@ const Carousel = () => {
         >
             {images.map((image, index) => (
                 <SwiperSlide key={index}>
-                    <img
-                        src={`src/assets/products/${image}`}
-                        alt='featured product'
-                    />
+                    <img src={image} alt='featured product' />
                 </SwiperSlide>
             ))}
         </Swiper>
