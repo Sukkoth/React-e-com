2a53b5c174eafade8fe61c23297d962e13886261
Sukkoth <Suukootj@gmail.com>
2023-08-23 21:16:31 +0300

    categories images added

--- src/components/Home/PopularCategories.jsx
+++ src/components/Home/PopularCategories.jsx
@@ -6,6 +6,7 @@ import {
     categoriesErrorSelector,
 } from '../../features/Categories/categoriesSlice';
 import ScaleLoader from 'react-spinners/ScaleLoader';
+import { PRODCTS_IMAGE_URL } from '../../config/env';
 
 const PopularCategories = () => {
     const categories = useSelector(categoriesSelector);
@@ -15,7 +16,7 @@ const PopularCategories = () => {
         ?.slice()
         .sort((a, b) => b?.productsCount - a?.productsCount)
         .slice(0, 6);
-
+    console.log('url', categories[1]?.imageUrl[0]);
     return (
         <div className='popular-categories'>
             <h4 className='title'> Popular Categories</h4>
@@ -24,7 +25,14 @@ const PopularCategories = () => {
                 topCategories?.map((category) => (
                     <div className='category' key={category?._id}>
                         <div className='img-container'>
-                            <img src={headhphone} alt='category_img' />
+                            <img
+                                src={
+                                    category?.imageUrl[0]
+                                        ? `${PRODCTS_IMAGE_URL}/${category?.imageUrl[0]}`
+                                        : headhphone
+                                }
+                                alt='category_img'
+                            />
                         </div>
                         <div className='details'>
                             <h3>{category?.name}</h3>
